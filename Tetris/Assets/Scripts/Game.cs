c65d6d64920a0c59268f8780f85b603e3eec885c
kamikadze_xdd <nazar.kulik99@gmail.com>
2018-12-02 22:47:59 +0200

    Add part of game

---
+++ Tetris/Assets/Scripts/Game.cs
@@ -0,0 +1,333 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    public static int gridWight = 10;
+    public static int gridHeight = 20;
+
+    public int scoreOneLine = 1;
+    public int scoreTwoLine = 2;
+    public int scoreThreeLine = 3;
+    public int scoreFourLine = 4;
+
+    private AudioSource audioSource;
+
+    private GameObject nextTetrisminoObj;
+
+    private string nextTetrismino;
+
+    private string currentTetrismino;
+
+    public AudioClip lineCrearedAudio;
+
+    public Text pub_score;
+
+    public int currentScore;
+
+    private int numberOfLinesThisTurn = 0;
+
+    private int scoreL = 0;
+    private int scoreJ = 0;
+    private int scoreSquare = 0;
+    private int scoreS = 0;
+    private int scoreZ = 0;
+    private int scoreLong = 0;
+    private int scoreT = 0;
+
+    public Text scoreTextL;
+    public Text scoreTextJ;
+    public Text scoreTextSquare;
+    public Text scoreTextS;
+    public Text scoreTextZ;
+    public Text scoreTextLong;
+    public Text scoreTextT;
+ 
+    public static Transform[,] grid = new Transform[gridWight, gridHeight];
+	// Use this for initialization
+	void Start () {
+        currentTetrismino = GetRandomTetrismino();
+
+        nextTetrismino = GetRandomTetrismino();
+
+        SpawnNextTetrismino();
+             
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    void Update ()
+    {
+        UpdateScore();
+        UpdateUI();
+    }
+
+
+    public void UpdateUI()
+    {
+        pub_score.text = currentScore.ToString();
+        scoreTextJ.text = scoreJ.ToString();
+        scoreTextL.text = scoreL.ToString();
+        scoreTextS.text = scoreS.ToString();
+        scoreTextZ.text = scoreZ.ToString();
+        scoreTextSquare.text = scoreSquare.ToString();
+        scoreTextT.text = scoreT.ToString();
+        scoreTextLong.text = scoreLong.ToString();
+    }
+
+    public void UpdateScore(){
+        if(numberOfLinesThisTurn > 0)
+        {
+            if(numberOfLinesThisTurn == 1)
+            {
+                ClearedOneLine();
+            }
+            else if(numberOfLinesThisTurn == 2)
+            {
+                ClearedTwoLine();
+            }
+            else if(numberOfLinesThisTurn == 3)
+            {
+                ClearedThreeLine();
+            }
+            else if(numberOfLinesThisTurn == 4)
+            {
+                ClearedFourLine();
+            }
+
+            PlayLineClearedAudio();
+
+            numberOfLinesThisTurn = 0;
+        }
+    }
+
+    public void ClearedOneLine()
+    {
+        currentScore += scoreOneLine;
+    }
+
+    public void ClearedTwoLine()
+    {
+        currentScore += scoreTwoLine;
+    }
+
+    public void ClearedThreeLine()
+    {
+        currentScore += scoreThreeLine;
+    }
+
+    public void ClearedFourLine()
+    {
+        currentScore += scoreFourLine;
+    }
+
+    public bool CheckIsAboveGrid(Tetrismino tetrismino)
+    {
+        for (int x = 0; x < gridWight; ++x)
+        {
+            foreach (Transform mino in tetrismino.transform) {
+                Vector2 pos = Round(mino.position);
+
+                if (pos.y > gridHeight - 1)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool IsFullRowAt (int y)
+    {
+        for (int x = 0; x < gridWight; ++x)
+        {
+            if (grid[x,y] == null)
+            {
+                return false;
+            }
+        }
+
+        numberOfLinesThisTurn++;
+
+        return true;
+    }
+
+    public void DeleteMinoAt(int y)
+    {
+        for (int x = 0; x < gridWight; ++x)
+        {
+            Destroy(grid[x, y].gameObject);
+
+            grid[x, y] = null; 
+        }
+    }
+
+    public void MoveRowDown(int y)
+    {
+        for (int x=0; x < gridWight; ++x)
+        {
+            if (grid[x,y] != null)
+            {
+                grid[x, y - 1] = grid[x, y];
+
+                grid[x, y] = null;
+
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    public void MoveAllRownDown(int y)
+    {
+        for (int i = y; i < gridHeight; ++i)
+        {
+            MoveRowDown(i);
+        }
+    }
+
+    public void DeleteRow()
+    {
+        for(int y = 0; y < gridHeight; ++y)
+        {
+            if (IsFullRowAt(y))
+            {
+                DeleteMinoAt(y);
+
+                MoveAllRownDown(y + 1);
+
+                --y;
+            }
+        }
+    }
+
+    public void UpdateGrid (Tetrismino tetrismino)
+    {
+        for (int y = 0; y < gridHeight; ++y){
+            for (int x = 0; x < gridWight; ++x)
+            {
+                if (grid[x,y] != null)
+                {
+                    if (grid[x,y].parent == tetrismino.transform)
+                    {
+                        grid[x, y] = null;
+                    }
+                }
+            }
+        }
+
+        foreach (Transform mino in tetrismino.transform)
+        {
+            Vector2 pos = Round(mino.position);
+
+            if (pos.y < gridHeight)
+            {
+                grid[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }
+
+    public Transform GetTransformAtGridPosition (Vector2 pos)
+    {
+        if (pos.y > gridHeight - 1)
+        {
+            return null;
+        }
+        else
+        {
+            return grid[(int)pos.x, (int)pos.y];
+        }
+    }
+
+    public void SpawnNextTetrismino()
+    {
+        GameObject nextTetromino = (GameObject)Instantiate(Resources.Load(currentTetrismino, typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
+
+        if(currentTetrismino == "Prefabs/Tetrismino_t")
+        {
+            scoreT++;
+        } else if (currentTetrismino == "Prefabs/Tetrismino_j")
+        {
+            scoreJ++;
+        } else if (currentTetrismino == "Prefabs/Tetrismino_s")
+        {
+            scoreS++;
+        } else if (currentTetrismino == "Prefabs/Tetrismino_z")
+        {
+            scoreZ++;
+        } else if (currentTetrismino == "Prefabs/Tetrismino_long")
+        {
+            scoreLong++;
+        } else if (currentTetrismino == "Prefabs/Tetrismino_square")
+        {
+            scoreSquare++;
+        } else if (currentTetrismino == "Prefabs/Tetrismino_l")
+        {
+            scoreL++;
+        }
+
+        currentTetrismino = nextTetrismino;
+
+        if (nextTetrisminoObj != null)
+        {
+            Destroy(nextTetrisminoObj);
+        }
+
+        nextTetrisminoObj = (GameObject)Instantiate(Resources.Load(nextTetrismino + "_next", typeof(GameObject)), new Vector2(20.0f, 14.0f), Quaternion.identity);
+
+        nextTetrismino = GetRandomTetrismino();
+    }
+
+    public bool CheckInsideGrid (Vector2 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWight && (int)pos.y >= 0);
+    }
+
+    public Vector2 Round (Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    string GetRandomTetrismino ()
+    {
+        int randomTetrismino = Random.Range(1, 8);
+        string randomTetrisminoName = "Prefabs/Tetrismino_t";
+
+        switch (randomTetrismino)
+        {
+            case 1:
+                randomTetrisminoName = "Prefabs/Tetrismino_t";
+                break;
+            case 2:
+                randomTetrisminoName = "Prefabs/Tetrismino_long";
+                break;
+            case 3:
+                randomTetrisminoName = "Prefabs/Tetrismino_square";
+                break;
+            case 4:
+                randomTetrisminoName = "Prefabs/Tetrismino_j";
+                break;
+            case 5:
+                randomTetrisminoName = "Prefabs/Tetrismino_l";
+                break;
+            case 6:
+                randomTetrisminoName = "Prefabs/Tetrismino_s";
+                break;
+            case 7:
+                randomTetrisminoName = "Prefabs/Tetrismino_z";
+                break;
+        }
+        return randomTetrisminoName;
+    }
+
+    public void GameOver ()
+    {
+        Application.LoadLevel("GameOver");
+    }
+
+    public void PlayLineClearedAudio()
+    {
+        audioSource.PlayOneShot(lineCrearedAudio);
+    }
+}
---
+++ Tetris/Assets/Scripts/Tetrismino.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetrismino : MonoBehaviour {
+
+    float fall = 0;
+
+    public float fallSpeed = 1;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+
+    public AudioClip moveSound;
+    public AudioClip rotateSound;
+    public AudioClip landSound;
+
+    private AudioSource audioSource;
+
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckUserInput();
+	}
+
+    void CheckUserInput () {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            if (CheckIsValidPosition())
+            {
+                FindObjectOfType<Game>().UpdateGrid(this);
+
+                PlayMoveAudio();
+            } else {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        } 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (CheckIsValidPosition())
+            {
+                FindObjectOfType<Game>().UpdateGrid(this);
+                PlayMoveAudio();
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (allowRotation)
+            {
+                if (limitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+                if (CheckIsValidPosition())
+                {
+                    FindObjectOfType<Game>().UpdateGrid(this);
+
+                    PlayRotateAudio();
+                }
+                else
+                {
+                    if (limitRotation)
+                    {
+                        if (transform.rotation.eulerAngles.z >= 90)
+                        {
+                            transform.Rotate(0, 0, -90);
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, -90);
+                        }
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (CheckIsValidPosition())
+            {
+                FindObjectOfType<Game>().UpdateGrid(this);
+
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    PlayMoveAudio();
+                }
+               
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+
+                FindObjectOfType<Game>().DeleteRow();
+
+                enabled = false;
+
+                if (FindObjectOfType<Game>().CheckIsAboveGrid(this))
+                {
+                    FindObjectOfType<Game>().GameOver();
+                }
+
+                PlayLandAudio();
+
+                FindObjectOfType<Game>().SpawnNextTetrismino();
+            }
+
+            fall = Time.time;
+        }
+    }
+
+    bool CheckIsValidPosition ()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+
+            if (FindObjectOfType<Game>().CheckInsideGrid (pos) == false)
+            {
+                return false;
+            }
+
+            if (FindObjectOfType<Game>().GetTransformAtGridPosition(pos) != null && FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void PlayMoveAudio()
+    {
+        audioSource.PlayOneShot(moveSound);
+    }
+
+    void PlayRotateAudio()
+    {
+        audioSource.PlayOneShot(rotateSound);
+    }
+
+    void PlayLandAudio()
+    {
+        audioSource.PlayOneShot(landSound);
+    }
+}
